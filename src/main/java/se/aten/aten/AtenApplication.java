56b61bdc028f653e262ddd4013ae499aae4380ba
Niklas Johansson <sirniklasjohansson@gmail.com>
2021-11-11 11:39:47 +0100

    adding project

---
+++ src/main/java/se/aten/aten/AtenApplication.java
@@ -0,0 +1,13 @@
+package se.aten.aten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/aten/aten/AtenApplicationTests.java
@@ -0,0 +1,13 @@
+package se.aten.aten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
