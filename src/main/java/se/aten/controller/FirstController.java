bd38dbb786c2779a8b2c38e9254e558eb5602d51
Emma Fredriksson <emfr87@gmail.com>
2021-11-15 09:41:23 +0100

    Add Controller and firstpage

---
+++ src/main/java/se/aten/controller/FirstController.java
@@ -0,0 +1,15 @@
+package se.aten.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FirstController {
+
+    @RequestMapping("/home.html")
+    public ModelAndView firstPage() {
+        return new ModelAndView("/WEB-INF/jsp/welcome.jsp");
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,1 @@
+<h1>Welcome to ATEN Webstore</h1>
