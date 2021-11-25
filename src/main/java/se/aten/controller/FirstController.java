02bf25d529b93d252c3184a12a966c243b2ad708
Tom Karlsson <tom_karlsson94@hotmail.com>
2021-11-25 15:45:29 +0100

    Add Unique fields, Refactor

--- src/main/java/se/aten/controller/FirstController.java
+++ src/main/java/se/aten/controller/FirstController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import se.aten.domain.Product;
+import se.aten.domain.ProductCategory;
+import se.aten.domain.ProductInventory;
 import se.aten.repository.ArticleRepository;
 
 import java.util.List;
@@ -18,8 +20,12 @@ public class FirstController {
     @RequestMapping("/home.html")
     public ModelAndView firstPage() {
 
-        Product firstArticle = new Product("apelem", 50.00);
-        Product secondArticle = new Product("appelem", 50.00);
+        ProductCategory game = new ProductCategory("Game", "Different kind of games");
+        ProductInventory bosse = new ProductInventory(100);
+        ProductInventory nisse = new ProductInventory(50);
+
+        Product firstArticle = new Product("apelem", "lol", "APLOL", 50.00, game, bosse);
+        Product secondArticle = new Product("appelem", "asda", "APASDA", 50.00, game, nisse);
         artRepo.save(firstArticle);
         artRepo.save(secondArticle);
         List<Product> allArticles = artRepo.findAll();
--- src/main/java/se/aten/domain/Product.java
+++ src/main/java/se/aten/domain/Product.java
@@ -1,49 +1,50 @@
 package se.aten.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.aten.repository.ArticleRepository;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
+@Table(name="tbl_Product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private int id;
     private String name;
+    private String description;
+    @Column(unique = true, nullable = false)
+    private String SKU;
+    @ManyToOne(cascade = CascadeType.PERSIST, optional = false)
+    @JoinColumn(name = "categoryId")
+    private ProductCategory categoryId;
+    @OneToOne(cascade = CascadeType.PERSIST, optional = false)
+    @JoinColumn(name = "inventoryId")
+    private ProductInventory inventoryId;
     private double price;
-
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "discountId")
+    private Discount discountId;
+    private Timestamp createdAt;
 
     public Product() {
     }
 
-    public Product(String name, double price) {
+    public Product(String name, String desc, String SKU, double price, ProductCategory productCategory, ProductInventory productInventory) {
         this.name = name;
+        this.description = desc;
+        this.SKU = SKU;
         this.price = price;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return this.price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
+        this.createdAt = new Timestamp(System.currentTimeMillis());
+        this.categoryId = productCategory;
+        this.inventoryId = productInventory;
+        // System.out.println(this.createdAt.getTime());        // För att få 16165771233114
     }
 
     @Override
     public String toString() {
-        return "Article: " + name  + ". Price: " + price;
+        return "Product: " + name  + ". Price: " + price;
     }
 }
