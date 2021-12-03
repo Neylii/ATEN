3410b96937b76e69ea9bb39a6597d0d8ba39b077
ShiftyGOSU <tom_karlsson94@hotmail.com>
2021-12-03 10:31:12 +0100

    Add CORS origin localhost

--- src/main/java/se/aten/rest/ProductRestController.java
+++ src/main/java/se/aten/rest/ProductRestController.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 /**
  * Rest API for products
  */
+@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/api")
 public class ProductRestController {
