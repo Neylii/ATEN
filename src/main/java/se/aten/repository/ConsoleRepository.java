003d81000fae289e57d7be9a87728a78ff0da270
Emma Fredriksson <emfr87@gmail.com>
2022-01-20 14:56:44 +0100

    Add functions to call api for more different types in order to click on category on frontend

--- src/main/java/se/aten/repository/ConsoleRepository.java
+++ src/main/java/se/aten/repository/ConsoleRepository.java
@@ -3,6 +3,11 @@ package se.aten.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import se.aten.domain.Console;
+import se.aten.domain.ConsoleType;
+
+
+import java.util.List;
 
 public interface ConsoleRepository extends JpaRepository<Console, Long> {
+    List<Console> findByBrand(ConsoleType brand);
 }
--- src/main/java/se/aten/rest/ProductRestController.java
+++ src/main/java/se/aten/rest/ProductRestController.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import se.aten.domain.Console;
-import se.aten.domain.Game;
-import se.aten.domain.Product;
+import se.aten.domain.*;
 import se.aten.repository.ConsoleRepository;
 import se.aten.repository.GameRepository;
 import se.aten.repository.ProductRepository;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +28,52 @@ public class ProductRestController {
     @Autowired
     private GameRepository gameRepo;
 
+    /**
+     * Finds all products, both games and consoles.
+     */
+    @GetMapping("/products")
+    public ResponseEntity getAllProducts() {
+        List<Product> allProducts = productRepo.findAll();
+        if (!allProducts.isEmpty()) {
+            return new ResponseEntity(allProducts, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Finds all products given its name.
+     * @param name
+     * @return a list of products with the specified name
+     */
+    @GetMapping("/products/{name}")
+    public ResponseEntity getProductByName(@PathVariable("name")String name) {
+        List<Product> allProductsByName = productRepo.findByName(name);
+        if (!allProductsByName.isEmpty()) {
+            return new ResponseEntity(allProductsByName, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Finds all consoles.
+     */
+    @GetMapping("/allConsoles")
+    public ResponseEntity getAllConsoles() {
+        List<Console> allConsoles = consoleRepo.findAll();
+        if (!allConsoles.isEmpty()) {
+            return new ResponseEntity(allConsoles, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Find a specific console given its ID.
+     * @param id
+     * @return the found console if any
+     */
     @GetMapping("/console/{id}")
     public ResponseEntity getConsoleById(@PathVariable("id") long id) {
         Optional<Console> console = consoleRepo.findById(id);
@@ -39,6 +84,39 @@ public class ProductRestController {
         }
     }
 
+    /**
+     * Finds all consoles of a specific brand.
+     * @param brand
+     * @return a list of consoles in the specific brand
+     */
+    @GetMapping("/consoles/{brand}")
+    public ResponseEntity getConsoleByBrand(@PathVariable("brand")ConsoleType brand) {
+        List<Console> allConsoleByBrand = consoleRepo.findByBrand(brand);
+        if (!allConsoleByBrand.isEmpty()) {
+            return new ResponseEntity(allConsoleByBrand, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Finds all games.
+     */
+    @GetMapping("/allGames")
+    public ResponseEntity getAllGames() {
+        List<Game> allGames = gameRepo.findAll();
+        if (!allGames.isEmpty()) {
+            return new ResponseEntity(allGames, HttpStatus.OK);
+        } else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Finds a specific game given its ID.
+     * @param id
+     * @return the found game if any
+     */
     @GetMapping("/game/{id}")
     public ResponseEntity getGameById(@PathVariable("id") long id) {
         Optional<Game> game = gameRepo.findById(id);
@@ -49,21 +127,16 @@ public class ProductRestController {
         }
     }
 
-    @GetMapping("/game")
-    public ResponseEntity getAllGames() {
-        List<Game> allGames = gameRepo.findAll();
-        if (!allGames.isEmpty()) {
-            return new ResponseEntity(allGames, HttpStatus.OK);
-        } else {
-            return new ResponseEntity(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/products")
-    public ResponseEntity getAllProducts() {
-        List<Product> allProducts = productRepo.findAll();
-        if (!allProducts.isEmpty()) {
-            return new ResponseEntity(allProducts, HttpStatus.OK);
+    /**
+     * Finds all games of a specific genre.
+     * @param genre
+     * @return a list of games in the specific genre
+     */
+    @GetMapping("/games/{genre}")
+    public ResponseEntity getGameByGenre(@PathVariable("genre") Genre genre) {
+        List<Game> allGamesByGenre = gameRepo.findByGenre(genre);
+        if (!allGamesByGenre.isEmpty()) {
+            return new ResponseEntity(allGamesByGenre, HttpStatus.OK);
         } else {
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
